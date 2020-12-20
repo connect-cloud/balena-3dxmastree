c920d94d0b1b0e2e8936202ae3b8a19315bc8000
connect-cloud <bart@connect-cloud.com>
2020-12-20 11:38:12 +0100

    add timer

--- 3dxmastree.py
+++ 3dxmastree.py
@@ -1,33 +1,53 @@
-#!/usr/bin/python3
-import time
-from signal import pause
+from time import sleep
+from datetime import datetime
+import os
+import signal
 
-
-
-try: #Check if python modules for gpiozero are installed
+try: # Check if python modules for gpiozero are installed
     from gpiozero import LEDBoard
     from gpiozero.tools import random_values
 except ImportError:
     exit("This script requires the gpiozero module\nInstall with: sudo apt-get install python3-gpiozero")
 
-def randomtree (delay):
-    tree = LEDBoard(*range(2,28),pwm=True)
-    try:
-        while True:
-            for led in tree:
-                led.source_delay = delay
-                led.source = random_values()
-            pause()
-    except (KeyboardInterrupt, SystemExit):
-        quit()
+# Get environment variables
+DELAY = float(os.environ["DELAY"]) if "DELAY" in os.environ else 1
+STARTTIME = os.environ["STARTTIME"] if "STARTTIME" in os.environ else "0000"
+STOPTIME = os.environ["STOPTIME"] if "STOPTIME" in os.environ else "2359"
 
 
-    
+
+
+class GracefulKiller:
+  kill_now = False
+  signals = {
+    signal.SIGINT: 'SIGINT',
+    signal.SIGTERM: 'SIGTERM'
+  }
+
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self, signum, frame):
+    print("\nReceived {} signal".format(self.signals[signum]))
+    print("Cleaning up resources. End of the program")
+    self.kill_now = True
+    quit()
+
+
 if __name__ == '__main__':
-    randomtree(1)
-
-
-
+    tree = LEDBoard(*range(2,28),pwm=True)
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        now = datetime.now().strftime("%H%M")
+        if now >= STARTTIME and now < STOPTIME:
+            for led in tree:
+                led.source_delay = DELAY
+                led.source = random_values()
+            signal.pause()
+        else:
+            tree.off()
+            sleep(60)
 
 
 
